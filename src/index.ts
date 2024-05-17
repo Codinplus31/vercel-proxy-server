ef9f996c820388b9d9eb22f0e7217ab2c84363b4
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-05-17 18:23:38 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 4000;
 
 app.use((req, res, next) => {
   console.log(
